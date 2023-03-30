3d7b8f46635afff33e4e2fa18095725ada31a988
jcm821 <jchangmin718@gmail.com>
2023-03-30 15:40:53 +0900

    ALLOWD_HOSTS 변경

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-2*un8v+s#hh3i*65=5!x96^93#)+9xwx)sty4%dr%*a+@#)8l6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['43.201.80.53']
 
 
 # Application definition
